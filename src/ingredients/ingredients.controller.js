ed8e32cc8f64333b3e1eff2f3479ec961135011f
dmb-2222 <dmb-2222@yandex.ru>
2020-04-23 00:53:54 +0300

    23/04/20

--- src/ingredients/ingredients.controller.js
+++ src/ingredients/ingredients.controller.js
@@ -1,3 +1,6 @@
+const mongoose = require("mongoose");
+
+
 const ingridientsModel = require("./ingredients.model");
 
 const createIngridients = async (req, res, next) => {
@@ -17,5 +20,32 @@ const createIngridients = async (req, res, next) => {
     next(err);
   }
 };
+
+const newValueIngridients = new ingridientsModel({
+  _id: new mongoose.Types.ObjectId(),
+  ingredients: [
+    { type: "ingredientId1", ref: "ingredients model name2" },
+    { type: "ingredientId2", ref: "ingredients model name2" },
+  ],
+});
+// const author = new Person({
+//   _id: new mongoose.Types.ObjectId(),
+//   name: 'Ian Fleming',
+//   age: 50
+// });
+
+// author.save(function (err) {
+//   if (err) return handleError(err);
+
+//   const story1 = new Story({
+//     title: 'Casino Royale',
+//     author: author._id    // assign the _id from the person
+//   });
+
+//   story1.save(function (err) {
+//     if (err) return handleError(err);
+//     // that's it!
+//   });
+// });
 
 module.exports = createIngridients;
